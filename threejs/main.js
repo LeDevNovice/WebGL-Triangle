1063263767da65fe5b764f7960cfd1759853f4ce
LeDevNovice <gregory.saison@proton.me>
2025-11-10 11:36:16 +0100

    feat: add camera options

--- threejs/main.js
+++ threejs/main.js
@@ -11,3 +11,21 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x1a1a26); // Set the background color of the scene to a dark gray (equivalent to gl.clearColor(0.1, 0.1, 0.15, 1.0) in WebGL)
 
 console.log("Scene created successfully !");
+
+/* SET UP CAMERA */
+console.log("Setting up camera...");
+
+// A camera is a device that will capture the scene and project it onto the screen
+// OrthographicCamera is a camera that will project the scene onto a 2D plane
+const camera = new THREE.OrthographicCamera(
+  -1,
+  1, // Left, right (width of the viewport)
+  1,
+  -1, // Top, bottom (height of the viewport)
+  0.1,
+  10 // Near, far (depth of the viewport)
+);
+
+camera.position.z = 1; // Set the position of the camera to the front of the scene
+
+console.log("Camera created successfully !");
