420652eefa1adc3a2ef6ef9506653bebfd052250
LeDevNovice <gregory.saison@proton.me>
2025-11-10 11:28:30 +0100

    feat: setting up the scene for threejs version

--- threejs/main.js
+++ threejs/main.js
@@ -1,1 +1,13 @@
 import * as THREE from "three";
+
+console.log("Starting Three.js...");
+
+/* SET UP SCENE */
+console.log("Setting up scene...");
+// A scene is a container that will hold all the objects
+// It is like a canvas in WebGL
+// But in WebGL, we need to create a canvas element and get the context of the canvas to start drawing
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1a1a26); // Set the background color of the scene to a dark gray (equivalent to gl.clearColor(0.1, 0.1, 0.15, 1.0) in WebGL)
+
+console.log("Scene created successfully !");
